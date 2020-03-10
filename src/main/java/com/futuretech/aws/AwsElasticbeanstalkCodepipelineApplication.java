3a0548c5a8d61af76096a5ee67824bcfb56c3e33
Sandeep Baniya <baniyasandy32@gmail.com>
2020-03-09 20:12:08 -0500

    second commit

--- src/main/java/com/futuretech/aws/AwsElasticbeanstalkCodepipelineApplication.java
+++ src/main/java/com/futuretech/aws/AwsElasticbeanstalkCodepipelineApplication.java
@@ -2,9 +2,10 @@ package com.futuretech.aws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AwsElasticbeanstalkCodepipelineApplication {
+public class AwsElasticbeanstalkCodepipelineApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AwsElasticbeanstalkCodepipelineApplication.class, args);
